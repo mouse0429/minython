bd163999183d281848c8f1a3edc5e31c6392ac02
min <mouse0429@naver.com>
2021-11-04 01:09:24 +0900

    211104 BOJ

--- BAEKJOON/11650.py
+++ BAEKJOON/11650.py
@@ -1,6 +1,6 @@
 import sys
 T=int(sys.stdin.readline())
-X=[(0,0) for _ in range(T)];
+X=[(0,0) for _ in range(T)]
 for i in range(T):
     x,y=map(int, sys.stdin.readline().split())
     X[i]=(x,y)
