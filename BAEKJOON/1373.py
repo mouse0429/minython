7925da4920f324d5fd89b3dc2a59aad9190b4d87
mouse0429 <mouse0429@naver.com>
2022-06-19 00:19:36 +0900

    BOJ 220619

---
+++ BAEKJOON/1373.py
@@ -0,0 +1,17 @@
+from cmath import e
+import math
+
+arr = list(map(int, input().strip()))
+arr.reverse()
+result = []
+
+for i in range(math.ceil(len(arr)/3)):
+    if (3*i+2) <= (len(arr)-1):
+        result.append(arr[3*i]+2*arr[3*i+1]+4*arr[3*i+2])
+    elif (3*i+1) <= (len(arr)-1):
+        result.append(arr[3*i]+2*arr[3*i+1])
+    else:
+        result.append(arr[3*i])
+
+result.reverse()
+print(''.join(str(_) for _ in result))
