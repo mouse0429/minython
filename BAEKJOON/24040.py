36856d8329e723a2fc37b606999a57ba132f8c41
민감자 <mouse0429@naver.com>
2025-01-22 14:22:26 +0900

    BOJ 250122

---
+++ BAEKJOON/24040.py
@@ -0,0 +1,9 @@
+import sys
+
+T = int(sys.stdin.readline())
+for _ in range(T):
+    N = int(sys.stdin.readline())
+    if N % 9 == 0 or N % 3 == 2:
+        print("TAK")
+    else:
+        print("NIE")
